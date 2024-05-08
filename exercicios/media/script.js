cb8a01880541a4d934971bf3b4a376fbc031a4b0
Camila Yukari Jodai <camijodai@gmail.com>
2024-05-08 13:48:14 -0300

    Média

---
+++ exercicios/media/script.js
@@ -0,0 +1,26 @@
+function calculateAverage() {
+    const nota1 = parseFloat(document.getElementById('nota1').value);
+    const nota2 = parseFloat(document.getElementById('nota2').value);
+    const nota3 = parseFloat(document.getElementById('nota3').value);
+    const nota4 = parseFloat(document.getElementById('nota4').value);
+
+    const average = (nota1 + nota2 + nota3 + nota4) / 4;
+    let status;
+
+    if (average < 4) {
+        document.getElementById('photo').src = "./img/reproved.jpg"
+        status = 'REPROVADO';
+    } else if (average >= 4 && average < 7) {
+        document.getElementById('photo').src = "./img/rec.jpg"
+        status = 'RECUPERAÇÃO';
+    } else {
+        document.getElementById('photo').src = "./img/aprovved.jpg"
+        status = 'APROVADO';
+    }
+
+    document.getElementById('result').innerText = `Média: ${average.toFixed(2)} - Status: ${status}`;
+}
+
+function limpar() {
+    document.getElementById('photo').src = "./img/average.jpg"
+}
